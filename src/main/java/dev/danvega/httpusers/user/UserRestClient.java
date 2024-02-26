07e18f8a705d173b11c6da6ad18b7c8790e16e89
Robert McNees <rmcnees@vmware.com>
2024-02-26 16:00:18 -0500

    Added imports and annotations for empty classes

--- src/main/java/dev/danvega/httpusers/user/UserRestClient.java
+++ src/main/java/dev/danvega/httpusers/user/UserRestClient.java
@@ -3,8 +3,10 @@ package dev.danvega.httpusers.user;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+@Component
 public class UserRestClient {
 
 }

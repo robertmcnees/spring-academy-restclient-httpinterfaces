1f1531e57db56bd2e317c61df5e2cb99446b377f
Robert McNees <rmcnees@vmware.com>
2024-02-26 15:55:20 -0500

    Added imports that will be used for UserRestClient

--- src/main/java/dev/danvega/httpusers/user/UserRestClient.java
+++ src/main/java/dev/danvega/httpusers/user/UserRestClient.java
@@ -1,4 +1,10 @@
 package dev.danvega.httpusers.user;
 
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
 public class UserRestClient {
+
 }

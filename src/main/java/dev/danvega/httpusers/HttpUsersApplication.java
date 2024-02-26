c8c4efc8eddbb042840aca87fb902199d01dcc72
Robert McNees <rmcnees@vmware.com>
2024-02-26 16:11:42 -0500

    Added imports to HttpUsersApplication

--- src/main/java/dev/danvega/httpusers/HttpUsersApplication.java
+++ src/main/java/dev/danvega/httpusers/HttpUsersApplication.java
@@ -1,7 +1,13 @@
 package dev.danvega.httpusers;
 
+import dev.danvega.httpusers.user.UserHttpClient;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class HttpUsersApplication {

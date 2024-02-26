40587535cc8fccb9282fdbb4bbf573bae0cab2f7
Robert McNees <rmcnees@vmware.com>
2024-02-26 14:54:26 -0500

    Initial commit of empty project

---
+++ src/main/java/dev/danvega/httpusers/HttpUsersApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.httpusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danvega/httpusers/HttpUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.httpusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
